5ccc1a83dff86057aa34581b65135d264f4f66cb
Logon Aluno <logonaluno@local.com>
2019-09-10 09:17:26 -0300

    Acertando o Lobo

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerScript : MonoBehaviour
 {
+    public bool moveClick;
     public NavMeshAgent playerNavMesh;
 
     private void Start()
@@ -13,7 +15,34 @@ public class PlayerScript : MonoBehaviour
 
     private void Update()
     {
-        SetDestination();
+        if (moveClick)
+        {
+            SetDestination();
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * velocidade);
+
+        an.SetFloat("Blend", Input.GetAxis("Vertical"));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            an.speed = 2;
+            velocidade = 4;
+        }
+        else
+        {
+            an.speed = 1;
+            velocidade = 2;
+        }
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * velocidade, 0, Space.Self);
     }
 
     private void SetDestination()
